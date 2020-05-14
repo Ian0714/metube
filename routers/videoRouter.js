78ab93ade06fdcfe37b1caf5c8db24cf76ba5978
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-05-14 17:38:50 +0900

    MVC3
    error length property

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-globalRouter.get(routes.videos, (req,res) => res.send('Videos'));
-globalRouter.get(routes.upload, (req,res) => res.send('Upload'));
-globalRouter.get(routes.videoDetail, (req,res) => res.send('Video Detail'));
-globalRouter.get(routes.editVideo, (req,res) => res.send('Edit Video'));
-globalRouter.get(routes.deleteVideo, (req,res) => res.send('Delete Video'));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
