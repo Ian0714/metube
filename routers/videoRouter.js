d8fc60ed535bc0f7a4ceec89c18920cf33fd7dd6
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-05-14 17:19:34 +0900

    MVC2
    Separating URL

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
-import express from "express"
+import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
+
+globalRouter.get(routes.videos, (req,res) => res.send('Videos'));
+globalRouter.get(routes.upload, (req,res) => res.send('Upload'));
+globalRouter.get(routes.videoDetail, (req,res) => res.send('Video Detail'));
+globalRouter.get(routes.editVideo, (req,res) => res.send('Edit Video'));
+globalRouter.get(routes.deleteVideo, (req,res) => res.send('Delete Video'));
 
 export default videoRouter;
